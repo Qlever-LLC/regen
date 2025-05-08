94d0f34bc9f08f5512ffb9597ec1e3d2c5e740bc
Alex Layton <alex@qlever.io>
2025-05-08 15:45:55 -0700

    chore: more stuff

--- routes/api/pdf/create.tsx
+++ routes/api/pdf/create.tsx
@@ -8,20 +8,20 @@ export const handler: Handlers = {
 	async GET(_req: Request, ctx: FreshContext) {
 		return await ctx.render();
 	},
-	async POST(req: Request, ctx: FreshContext) {
+	async POST(req: Request, _ctx: FreshContext) {
 		const data = await req.formData();
 		const browser = await (BROWSER
 			? connect({ wsEndpoint: BROWSER })
-			: launch());
+			: launch({ cache: "/tmp/astral" }));
 		try {
 			// TODO: Actually make PDF template page
-			const page = await browser.newPage(ctx.url + `template?data=${data}`);
+			const page = await browser.newPage(`https://www.google.com?q=${data}`);
 			const pdf = await page.pdf();
 			// TODO: Sign the PDF
 			return new Response(pdf, { status: 201 });
 		} finally {
 			if (!BROWSER) {
-				await browser.close();
+				//await browser.close();
 			}
 		}
 	},
