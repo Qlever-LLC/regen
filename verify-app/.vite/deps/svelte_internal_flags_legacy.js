e758ab6b5647bf44b02b8ef72ab5accaf251d22e
Sam Noel <noel.samuel.a@gmail.com>
2025-05-13 15:35:23 -0400

    fix: build changes

--- verify-app/.vite/deps/svelte_internal_flags_legacy.js
+++
@@ -1,8 +0,0 @@
-import {
-  enable_legacy_mode_flag
-} from "./chunk-JF7JQTRA.js";
-import "./chunk-NNIHVWYK.js";
-
-// node_modules/.deno/svelte@5.28.3/node_modules/svelte/src/internal/flags/legacy.js
-enable_legacy_mode_flag();
-//# sourceMappingURL=svelte_internal_flags_legacy.js.map
