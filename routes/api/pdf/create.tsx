ef7f71384101e1456c7f2555f6e8eb47f07437a4
Alex Layton <alex@qlever.io>
2025-05-12 18:10:31 -0700

    feat: add pdf signing code
    The code is not tested

--- routes/api/pdf/create.tsx
+++ routes/api/pdf/create.tsx
@@ -1,7 +1,12 @@
 import { FreshContext, Handlers } from "$fresh/server.ts";
 
 import { PDFDocument } from "pdf-lib";
+import { pdflibAddPlaceholder } from "@signpdf/placeholder-pdf-lib";
+import signpdf from "@signpdf/signpdf";
+import { P12Signer } from "@signpdf/signer-p12";
 
+// TODO: Actually try to sign the PDF
+const cert = Deno.env.get("P12_CERT_PATH");
 const template = await Deno.readFile("static/template.pdf");
 
 export const handler: Handlers = {
@@ -10,6 +15,15 @@ export const handler: Handlers = {
 	},
 	async POST(req: Request, _ctx: FreshContext) {
 		const doc = await PDFDocument.load(template);
+		// FIXME: Probably should just have the placeholder builtin to template
+		pdflibAddPlaceholder({
+			pdfDoc: doc,
+			reason: "test",
+			contactInfo: "test@qlever.io",
+			name: "test",
+			location: "here",
+		});
+
 		const form = doc.getForm();
 		const fields = form.getFields();
 		const data = await req.formData();
@@ -19,19 +33,24 @@ export const handler: Handlers = {
 				//TODO: Non-text field support
 				const text = form.getTextField(name);
 				text.setText(data.get(name)?.toString());
-			} catch {
-				console.warn(`Failed to handle form field ${name}`);
+			} catch (error: unknown) {
+				console.warn(error, `Failed to handle form field ${name}`);
 			}
 		}
+
+		form.flatten();
 		const filled = await doc.save();
-		// TODO: Sign the PDF
-		return new Response(filled, {
-			status: 201,
-			headers: {
-				"Content-Type": "application/pdf",
-				"Content-Disposition": 'attachment; filename="certification.pdf',
+		const signer = cert ? new P12Signer(await Deno.readFile(cert)) : undefined;
+		return new Response(
+			signer ? await signpdf.default.sign(filled, signer) : filled,
+			{
+				status: 201,
+				headers: {
+					"Content-Type": "application/pdf",
+					"Content-Disposition": 'attachment; filename="certification.pdf',
+				},
 			},
-		});
+		);
 	},
 };
 
